c682f8599ab4e658afc78fd9257805c2fbf1ccd9
Denis <denis@ibm.com>
2022-05-12 22:41:01 -0300

    chore: :zap: Add GraphQL simple base

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -3,16 +3,19 @@ import 'express-async-errors';
 import express, { Request, Response, NextFunction } from 'express';
 import * as dotenv from 'dotenv';
 import { AppError } from '@shared/errors/AppError';
-import { router } from '@shared/infra/http/routes';
+import { router } from '@shared/infra/http/api/routes';
 
 import '@shared/container';
+import { graphqlRouter } from './graphql/graphql';
 
 const app = express();
+
 dotenv.config();
 
 app.use(express.json());
 
 app.use(router);
+app.use(graphqlRouter);
 
 app.use(
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
